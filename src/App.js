ba1366aff74ebadde13c6a85f47c5d90b36a9144
Kun <yukun0620@gmail.com>
2021-10-22 02:15:12 +0800

    practice1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Container, Row, Col, Button, Alert, Breadcrumb, Card, Form } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Container>
+          <Form>
+            <Row>
+              <Col md>
+                <Form.Group controlId='formEmail'>
+                  <Form.Label>Email Address</Form.Label>
+                  <Form.Control type='email' placeholder='Example@gmail.com'></Form.Control>
+                  <Form.Text className='text-muted'>
+                    We'll never share your email address, trust us!
+                  </Form.Text>
+                </Form.Group>
+              </Col>
+              <Col md>
+                <Form.Group controlId='formPassword'>
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type='password' placeholder='password'></Form.Control>
+                </Form.Group>
+              </Col>
+            </Row>
+            <Button type='submit' variant='secondary'>Login</Button>
+          </Form>
+          <Card className='mb-3' style={{color: 'black'}}>
+            <Card.Img src='https://picsum.photos/200/50'/>
+            <Card.Body>
+              <Card.Title>YOYO</Card.Title>
+              <Card.Text>This is an example of react bootstrap</Card.Text>
+              <Button>Read More</Button>
+            </Card.Body>
+          </Card>
+          <Breadcrumb>
+            <Breadcrumb.Item>Test1</Breadcrumb.Item>
+            <Breadcrumb.Item>Test2</Breadcrumb.Item>
+            <Breadcrumb.Item active>Test3</Breadcrumb.Item>
+          </Breadcrumb>
+          <Alert variant="success">This is a button</Alert>
+          <Button>Test Button</Button>
+        </Container>
       </header>
     </div>
   );
